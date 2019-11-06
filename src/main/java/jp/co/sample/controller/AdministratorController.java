cbcfeed6f8df547bbd77bc619eb8f1f394260ed3
yuki-and <yuki.andoooo@gmail.com>
2019-11-06 15:16:36 +0900

    ログインメソッドの実装

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 /**
@@ -30,6 +31,11 @@ public class AdministratorController {
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
+	}
+	
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
 	}
 	
 	/**
@@ -58,4 +64,8 @@ public class AdministratorController {
 		return "redirect:/";
 	}
 	
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+	}
 }
